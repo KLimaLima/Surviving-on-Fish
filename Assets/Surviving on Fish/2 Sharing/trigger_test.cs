299fe523895c0827cdbaa42b3b05377ae48ee637
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-01-02 17:38:22 +0800

    adding system

--- Assets/Surviving on Fish/2 Sharing/trigger_test.cs
+++ Assets/Surviving on Fish/2 Sharing/trigger_test.cs
@@ -3,21 +3,23 @@ using System.Collections;
 
 public class trigger_test : MonoBehaviour
 {
+    private GameData data;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //data = GameData.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(data.amountGive.ToString());
     }
     // “other” refers to the collider on the GameObject inside this trigger
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("A collider has entered the DoorObject trigger");
+        GameData.Instance.amountGive += 1;
     }
 
     void OnTriggerStay(Collider other)
@@ -27,6 +29,7 @@ public class trigger_test : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("A collider has exited the DoorObject trigger");
+        GameData.Instance.amountGive -= 1;
+        // Debug.Log(GameData.Instance.amountGive);
     }
 }
