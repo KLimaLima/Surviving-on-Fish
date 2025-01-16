b9ed0faab07e89ea4d0a58a13ecbec31f614890a
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-01-16 16:40:08 +0800

    fish spawned correctly

--- Assets/Surviving on Fish/1 Fishing/fish_spawner.cs
+++ Assets/Surviving on Fish/1 Fishing/fish_spawner.cs
@@ -6,25 +6,37 @@ public class fish_spawner : MonoBehaviour
     [SerializeField] public GameObject fishPrefab;
     private bool canSpawn;
     private Collider areaToSpawn;
-
-    [SerializeField] public Vector3 objectPosition;
+    private float totalSeconds;
+    [SerializeField] public float maxTime;
+    [SerializeField] public float rarity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         canSpawn = false;
         areaToSpawn = GetComponent<Collider>();
+        totalSeconds = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        totalSeconds += Time.fixedDeltaTime;
+        
         if (!canSpawn) { return; }
 
-        if (Random.Range(0, 100) < 90) { return; }
+        if (totalSeconds < maxTime) { return; }
+
+        if (Random.Range(0, 100) > rarity)
+        {
+            totalSeconds = 0;
+            return;
+        }
 
         Vector3 randomPosition = new Vector3(Random.Range(areaToSpawn.bounds.min.x, areaToSpawn.bounds.max.x), (areaToSpawn.bounds.max.y + 0.1f), Random.Range(areaToSpawn.bounds.min.z, areaToSpawn.bounds.max.z));
         Instantiate(fishPrefab, randomPosition, Quaternion.identity);
+        totalSeconds = 0;
+
     }
 
     // “other” refers to the collider on the GameObject inside this trigger
