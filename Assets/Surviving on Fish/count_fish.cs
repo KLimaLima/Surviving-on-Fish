3c27e5c6ffcbc8c782127a1a495b703441d8ac65
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-01-17 17:26:10 +0800

    count fish only counts object with fish tags
    Sharing scene;
    changed script name and arrangement;

--- Assets/Surviving on Fish/count_fish.cs
+++ Assets/Surviving on Fish/count_fish.cs
@@ -1,11 +1,7 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
-public class trigger_test : MonoBehaviour
+public class count_fish : MonoBehaviour
 {
-
-    //List<GameObject> fishObjects = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,24 +12,31 @@ public class trigger_test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(GameData.Instance.amountGive);
+        Debug.Log(GameData.Instance.amountGive);
     }
 
     // “other” refers to the collider on the GameObject inside this trigger
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("A collider has entered the DoorObject trigger");
+        //Debug.Log("A collider has entered the DoorObject trigger");
+
+        // If not fish tag then finish
+        if (other.tag != "Fish") { return; }
+
         GameData.Instance.amountGive += 1;
         GameData.Instance.fishObjects.Add(other.gameObject);
     }
 
     void OnTriggerStay(Collider other)
     {
-        Debug.Log("A collider is inside the DoorObject trigger");
+        //Debug.Log("A collider is inside the DoorObject trigger");
     }
 
     void OnTriggerExit(Collider other)
     {
+        // If not fish tag then finish
+        if (other.tag != "Fish") { return; }
+
         GameData.Instance.amountGive -= 1;
         GameData.Instance.fishObjects.Remove(other.gameObject);
         // Debug.Log(GameData.Instance.amountGive);
